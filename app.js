2fa642d0f6ce3b8a73101e2197cb73cce72e229e
shukhrat <sadullayevsh24@gmail.com>
2023-08-09 12:56:58 +0500

    it is not important!

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/todolistDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true, // Add this option for Mongoose 6.0 and later
 });
-
 const itemsSchema = new mongoose.Schema({
   name: String,
 });
@@ -28,26 +27,41 @@ const item2 = new Item({
 const item3 = new Item({
   name: '<-- Hit this to delete an item.'
 })
+
 const defaultItems = [item1, item2, item3];
 
 (async () => {
   try {
-    await Item.insertMany(defaultItems);
-    console.log('Default items inserted successfully!');
+    // Check if default items exist in the database
+    const existingItems = await Item.find({});
+
+    // If there are no items, insert the defaultItems
+    if (existingItems.length === 0) {
+      await Item.insertMany(defaultItems);
+      console.log('Default items inserted successfully!');
+    } else {
+      console.log('Default items already exist in the database. Skipping insertion.');
+    }
   } catch (err) {
     console.error('Error inserting default items:', err);
   }
   // Now start the server
-  app.listen(3000, function () {
-    console.log("Server started on port 3000");
+  app.listen(3001, function () {
+    console.log("Server started on port 3001");
   });
 })();
 
-app.get("/", function (req, res) {
 
-  res.render("list", { listTitle: 'Today', newListItems: defaultItems });
-
+app.get("/", async function (req, res) {
+  try {
+    const foundItems = await Item.find({});
+    res.render("list", { listTitle: 'Today', newListItems: foundItems });
+  } catch (err) {
+    console.error(err);
+    // Handle the error and respond accordingly
+  }
 });
+
 
 app.post("/", function (req, res) {
 
@@ -73,6 +87,3 @@ app.get("/about", function (req, res) {
 
 
 
-mongoose.connection.on('error', (err) => {
-  console.error('MongoDB connection error:', err);
-});
